f28eb8904c6e73b1b19fef82eb9d61ec96a31190
Ilya <@iIlyaM>
2024-05-09 22:53:03 +0300

    Add emits (not work yet)

--- components/form/login.vue
+++ components/form/login.vue
@@ -3,33 +3,36 @@ const props = defineProps({
     formToggle: Boolean
 })
 
+const emit = defineEmits(['changeToogle'])
+
 </script>
 <template>
-    <div class="form-box login" :class="{ 'active': !formToggle }">
-        <h2>Логин</h2>
-        <form action="#">
-            <div class="input-box">
-                <span class="icon">
-                    <ion-icon name="mail"></ion-icon>
-                </span>
-                <input type="email" required>
-                <label>Электронная почта</label>
-            </div>
-            <div class="input-box">
-                <span class="icon">
-                    <ion-icon name="lock"></ion-icon>
-                </span>
-                <input type="password" required>
-                <label>Пароль</label>
-            </div>
-            <button type="submit" class="login-btn">Войти</button>
-            <div class="login-register">
-                <p>Нет аккаунта? Зарегистрируйтесь!
-                    <span @click="toFormToggle" class="register-link">
-                        Регистрация!
+        <div class="form-box login" :class="{ 'active': !formToggle }">
+            <h2>Логин</h2>
+            <form action="#">
+                <div class="input-box">
+                    <span class="icon">
+                        <ion-icon name="mail"></ion-icon>
                     </span>
-                </p>
-            </div>
-        </form>
-    </div>
+                    <input type="email" required>
+                    <label>Электронная почта</label>
+                </div>
+                <div class="input-box">
+                    <span class="icon">
+                        <ion-icon name="lock"></ion-icon>
+                    </span>
+                    <input type="password" required>
+                    <label>Пароль</label>
+                </div>
+                <button type="submit" class="login-btn">Войти</button>
+                <div class="login-register">
+                    <p>Нет аккаунта? Зарегистрируйтесь!
+                        <span @click="$emit('changeToogle', formToggle)" class="register-link">
+                            Регистрация!
+                        </span>
+                    </p>
+                </div>
+            </form>
+        </div>
 </template>
+<style src="~/components/form/__form.css" lang="css"></style>
--- components/form/registration.vue
+++ components/form/registration.vue
@@ -1,8 +1,79 @@
 <script setup>
+const props = defineProps({
+    formToggle: Boolean
+})
+
+const emit = defineEmits(['changeToogle'])
 
 </script>
 <template>
-    <div class="form-box login">
-        <slot></slot>
+
+    <Header />
+
+    <div class="body">
+        <div class="wrapper">
+            <span class="icon-close">
+                <ion-icon name="close-circle"></ion-icon>
+            </span>
+
+            <Transition>
+
+                <FormLogin v-if="!formToggle" :formToggle="!formToggle" />
+
+            </Transition>
+
+
+            <Transition v-if="formToggle">
+                <div class="form-box register" :class="{ 'active': formToggle }">
+                    <h2>Регистрация</h2>
+                    <form action="#">
+                        <div class="input-box">
+                            <span class="icon">
+                                <ion-icon name="mail"></ion-icon>
+                            </span>
+                            <input type="email" required>
+                            <label>Электронная почта</label>
+                        </div>
+                        <div class="input-box">
+                            <span class="icon">
+                                <ion-icon name="lock"></ion-icon>
+                            </span>
+                            <input type="password" required>
+                            <label>Пароль</label>
+                        </div>
+                        <div class="input-box">
+                            <span class="icon">
+                                <ion-icon name="last-name"></ion-icon>
+                            </span>
+                            <input type="text" required>
+                            <label>Фамилия</label>
+                        </div>
+                        <div class="input-box">
+                            <span class="icon">
+                                <ion-icon name="first-name"></ion-icon>
+                            </span>
+                            <input type="text" required>
+                            <label>Имя</label>
+                        </div>
+                        <div class="input-box">
+                            <span class="icon">
+                                <ion-icon name="patronymic"></ion-icon>
+                            </span>
+                            <input type="text" required>
+                            <label>Отчество</label>
+                        </div>
+                        <button type="submit" class="login-btn">Войти</button>
+                        <div class="login-register">
+                            <p>Уже есть аккаунт? Войдите!
+                                <span @click="$emit('changeToogle', formToggle)" class="login-link">
+                                    Вход
+                                </span>
+                            </p>
+                        </div>
+                    </form>
+                </div>
+            </Transition>
+        </div>
     </div>
 </template>
+<style src="~/components/form/__form.css" lang="css"></style>
